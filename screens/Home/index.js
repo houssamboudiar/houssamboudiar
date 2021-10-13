bd3ef7ac6f7bc341e3485899eecc55fff12ee344
houssamboudiar <houssam.boudiar0107@gmail.com>
2021-10-13 12:43:07 +0100

    fixed export bug

--- screens/Home/index.js
+++ screens/Home/index.js
@@ -1,6 +1,6 @@
 export { default as Hero } from './Hero';
-// export { default as Works } from './Works';
-// export { default as Information } from './Information';
-// export { default as Testimonials } from './Testimonials';
-// export { default as Facts } from './Facts';
-// export { default as InformationII } from './InformationII';
+export { default as Works } from './Works';
+export { default as Information } from './Information';
+export { default as Testimonials } from './Testimonials';
+export { default as Facts } from './Facts';
+export { default as InformationII } from './InformationII';
